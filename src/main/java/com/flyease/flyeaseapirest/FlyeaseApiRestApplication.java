e534276033a33a0813e9ba4b51202af70158a899
Diego Andrés Castro <diegocastroac123@gmail.com>
2024-03-18 15:17:15 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/flyease/flyeaseapirest/FlyeaseApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.flyease.flyeaseapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyeaseApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyeaseApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flyease/flyeaseapirest/FlyeaseApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyease.flyeaseapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyeaseApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
